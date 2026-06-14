ae38a211a922f2bbc3117c56d4216c435de496fe
Alan-Robles <alanroblesgracia@gmail.com>
2026-06-13 21:42:29 -0600

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,9 @@
+# gunicorn.conf.py
 timeout = 120
 workers = 1
 worker_class = "sync"
+
+def post_fork(server, worker):
+    """Se ejecuta en el worker DESPUÉS de que el puerto ya está ligado."""
+    from dashboard2026 import load_data
+    load_data()
